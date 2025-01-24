08b4ffce0710fd9f49650edc79cc84ac018c6432
figgd <mael.humez@gmail.com>
2025-01-24 14:54:43 +0100

    fix

--- FrontEnd/js/modaal.js
+++ FrontEnd/js/modaal.js
@@ -1,21 +1,8 @@
-<<<<<<< HEAD
-//MODAL//
-
-=======
->>>>>>> 062b23d (update)
 const modal = document.querySelector('#modal');
 const modalContent = document.querySelector('#modal-content');
 const modalPhoto = document.querySelector('#modal-photo');
 const modalClose = document.querySelector('#modal-close');
 
-<<<<<<< HEAD
-function showModal() {
-  modal.style.display = 'block';
-}
-
-function hideModal() {
-  modal.style.display = 'none';
-=======
 
 function showModal() {
   modal.style.display = 'block';
@@ -45,7 +32,6 @@ function hideModal() {
   document.getElementById("form-photo-div > p").style.display = "block";
   document.getElementById("image").style.display = "block";
   document.getElementById("iModalImage").style.display = "block";
->>>>>>> 062b23d (update)
 }
 
 modalContent.addEventListener('click', function(e) {
@@ -57,26 +43,13 @@ modalPhoto.addEventListener('click', function(e) {
 
 modalClose.addEventListener('click', hideModal);
 
-<<<<<<< HEAD
-
-modal.addEventListener('click', hideModal);
-
-
-//Bouton Photo//
-
-=======
 modal.addEventListener('click', hideModal);
 
 // Ouvrir Modal
->>>>>>> 062b23d (update)
 const newPhotoBtn = document.querySelector('#new-photo');
 const returnBtn = document.querySelector('#modal-return');
 const modalPhotoClose = document.querySelector("#modal-photo-close");
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 062b23d (update)
 newPhotoBtn.addEventListener('click', function() {
   modalContent.style.display = 'none';
   modalPhoto.style.display = 'block';
@@ -85,54 +58,6 @@ newPhotoBtn.addEventListener('click', function() {
 returnBtn.addEventListener('click', function(){
   modalContent.style.display = 'flex';
   modalPhoto.style.display = 'none';
-<<<<<<< HEAD
-})
-
-modalPhotoClose.addEventListener('click', hideModal);
-
-document.addEventListener("DOMContentLoaded", () => {
-
-  function showModal() {
-    document.getElementById("modal").style.display = "block";
-  }
-
-
-  document.getElementById("modal-close").addEventListener("click", function() {
-    document.getElementById("modal").style.display = "none";
-  });
-
-
-  document.getElementById("modal-photo-close").addEventListener("click", function() {
-    document.getElementById("modal-photo").style.display = "none";
-  });
-
-
-  document.getElementById("modal-return").addEventListener("click", function() {
-    document.getElementById("modal-photo").style.display = "none";
-  });
-
-
-  document.querySelector("#modal-trigger").addEventListener("click", showModal);
-});
-
-const imagesModalContainer = document.querySelector('.gallery-modal')
-
-function createModalWorkFigure(work) {
-  const figure = document.createElement('figure')
-  const figureCaption = document.createElement('figcaption')
-  const figureImage = document.createElement('img')
-  const deleteIcon = document.createElement('i') 
-        
-  figureImage.src = work.imageUrl
-  figureImage.alt = work.title
-  figure.setAttribute('data-id', work.id); 
-  deleteIcon.className = "fa-regular fa-trash-can" 
-
-  figure.appendChild(figureImage)
-  figure.appendChild(figureCaption)
-  figure.appendChild(deleteIcon)
-
-=======
 });
 
 modalPhotoClose.addEventListener('click', hideModal);
@@ -160,7 +85,6 @@ function createModalWorkFigure(work) {
   figure.appendChild(figureImage);
   figure.appendChild(figureCaption);
   figure.appendChild(deleteIcon);
->>>>>>> 062b23d (update)
 
   deleteIcon.addEventListener('click', (event) => {
     event.preventDefault();
@@ -184,33 +108,20 @@ const categorySelect = document.getElementById('modal-photo-category');
 const imageInput = document.getElementById('image');
 const submitButton = document.getElementById('modal-valider');
 
-<<<<<<< HEAD
-=======
 // Fonction pour vérifier l'état du formulaire
->>>>>>> 062b23d (update)
 function checkForm() {
   if (titleInput.value !== '' && categorySelect.value !== '' && imageInput.value !== '') {
     submitButton.style.backgroundColor = '#1D6154';
   } else {
     submitButton.style.backgroundColor = '';
-<<<<<<< HEAD
-    }
-  }
-=======
   }
 }
->>>>>>> 062b23d (update)
 
 titleInput.addEventListener('input', checkForm);
 categorySelect.addEventListener('change', checkForm);
 imageInput.addEventListener('change', checkForm);
 
-<<<<<<< HEAD
-//Ajouter l'image//
-
-=======
 // Ajouter à modal
->>>>>>> 062b23d (update)
 const btnValider = document.getElementById("modal-valider");
 btnValider.addEventListener("click", addNewWork);
 
@@ -223,28 +134,15 @@ function addNewWork(event) {
   const category = document.getElementById("modal-photo-category").value;
   const image = document.getElementById("image").files[0];
 
-<<<<<<< HEAD
-
-  if(!title || !category || !image) {
-    alert('Veuillez remplir tous les champs du formulaire.')
-    return;
-  }
-
-  //Check si la taille est correcte//
-=======
   if (!title || !category || !image) {
     alert('Veuillez remplir tous les champs du formulaire.');
     return;
   }
 
->>>>>>> 062b23d (update)
   if (image.size > 4 * 1024 * 1024) {
     alert("La taille de l'image ne doit pas dépasser 4 Mo.");
     return;
   }
-<<<<<<< HEAD
-  
-=======
 
   if (!image.type.startsWith("image/")) {
     alert("Impossible veuillez télécharger une image.");
@@ -254,7 +152,6 @@ function addNewWork(event) {
   const modal = document.getElementById("modal");
   modal.style.display = "none";
 
->>>>>>> 062b23d (update)
   const formData = new FormData();
   formData.append("title", title);
   formData.append("category", category);
@@ -274,29 +171,16 @@ function addNewWork(event) {
     const figure = createWorkFigure(work);
     const gallery = document.querySelector('.gallery');
     gallery.appendChild(figure);
-<<<<<<< HEAD
-  
-=======
->>>>>>> 062b23d (update)
 
     const figureModal = createModalWorkFigure(work);
     const galleryModal = document.querySelector('.gallery-modal');
     galleryModal.appendChild(figureModal);
-<<<<<<< HEAD
-  
-    alert('Le nouvel travail a été ajouté avec succès.');
-  })
-  .catch(error => console.error(error));
-}
-//Supprimer//
-=======
 
     alert('Le nouvel travail a été ajouté avec succès.');
     hideModal();  
   })
   .catch(error => console.error(error));
 }
->>>>>>> 062b23d (update)
 
 function deleteWorkById(workId) {
     const token = sessionStorage.getItem("Token");
@@ -310,29 +194,6 @@ function deleteWorkById(workId) {
         }
       })
       .then(response => {
-<<<<<<< HEAD
-        if (!response.ok){
-        throw new error ('La supression du travai à echoué.');
-      }
-      const modalWorkToRemove = document.querySelector(`figure[data-id="${workId}"]`);
-      if (modalWorkToRemove) {
-        modalWorkToRemove.remove();
-        
-      const galleryWorkToRemove = document.querySelector(`figure[data-id="${workId}"]`);
-      if (galleryWorkToRemove) {
-          galleryWorkToRemove.remove();
-      } else {
-          console.error('Élément à supprimer non trouvé dans la galerie principale');
-        }
-      } else {
-          console.error('Élément à supprimer non trouvé dans la modale');
-      }
-    })
-    .catch(error => console.error(error));
-    }    
-  }  
-//PREVIEW IMAGE//
-=======
         if (!response.ok) {
           throw new Error('La suppression du travail a échoué.');
         }
@@ -352,7 +213,6 @@ function deleteWorkById(workId) {
     }    
 }
 
->>>>>>> 062b23d (update)
 const inputImage = document.getElementById("image");
 const labelImage = document.getElementById("label-image");
 const pImage = document.querySelector("#form-photo-div > p");
@@ -369,14 +229,8 @@ inputImage.addEventListener("change", function () {
   labelImage.style.display = "none";
   pImage.style.display = "none";
   inputImage.style.display = "none";
-<<<<<<< HEAD
-  iModalImage.style.display = "none";
-  document.getElementById("form-photo-div").appendChild(imgPreview);
-});
-=======
   iconeImage.style.display = "none";
   document.getElementById("form-photo-div").appendChild(imgPreview);
 });
 
 document.getElementById('modal-photo-close').addEventListener('click', hideModal);
->>>>>>> 062b23d (update)
